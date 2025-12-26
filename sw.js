26530a415c21cd36fa48e472b0740d44154a5580
unknown <mguimarcos39@gmail.com>
2025-12-26 08:50:18 -0300

    Adicionando app para celular

--- sw.js
+++ sw.js
@@ -1,40 +1,43 @@
---- /dev/null
-+++ c:\Users\santa fe\Desktop\chamados-ti\sw.js
-@@ -0,0 +1,36 @@
-+const CACHE_NAME = 'chamados-ti-v1';
-+const ASSETS_TO_CACHE = [
-+  './',
-+  './index.html',
-+  './login.html',
-+  './consulta.html',
-+  './css/style.css',
-+  './js/index.js',
-+  './js/theme-switcher.js',
-+  './assets/logo.jpg'
-+];
-+
-+// Instalação: Cache dos arquivos estáticos principais
-+self.addEventListener('install', (event) => {
-+  event.waitUntil(
-+    caches.open(CACHE_NAME).then((cache) => {
-+      return cache.addAll(ASSETS_TO_CACHE);
-+    })
-+  );
-+});
-+
-+// Estratégia: Network First (Tenta a internet, se cair usa o cache)
-+self.addEventListener('fetch', (event) => {
-+  event.respondWith(
-+    fetch(event.request)
-+      .then((response) => {
-+        // Atualiza o cache com a nova versão se a rede funcionar
-+        const responseClone = response.clone();
-+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
-+        return response;
-+      })
-+      .catch(() => {
-+        // Se estiver offline, retorna o que tiver no cache
-+        return caches.match(event.request);
-+      })
-+  );
-+});
+const CACHE_NAME = 'chamados-ti-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './login.html',
+  './consulta.html',
+  './painel.html',
+  './css/style.css',
+  './js/index.js',
+  './js/login.js',
+  './js/consulta.js',
+  './js/painel.js',
+  './js/firebase-init.js',
+  './js/utils.js',
+  './js/theme-switcher.js',
+  './assets/logo.jpg'
+];
+
+// Instalação: Cache dos arquivos estáticos principais
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Estratégia: Network First (Tenta a internet, se cair usa o cache)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // Atualiza o cache com a nova versão se a rede funcionar
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+        return response;
+      })
+      .catch(() => {
+        // Se estiver offline, retorna o que tiver no cache
+        return caches.match(event.request);
+      })
+  );
+});
